c646ef4a83935fe840209c1df877f704330cd923
赵猛 <zhaomeng@zhaomengdeMacBook-Pro.local>
2017-12-26 00:32:00 +0800

    hmr && dev-server

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
-
-module.exports = {
+const webpack = require('webpack');
+const isDev = process.env.NODE_ENV === 'development';
+const config = {
     entry: {
         app: path.join(__dirname, '../client/app.js')
     },
     output: {
         filename: '[name].[hash].js',
         path: path.join(__dirname, '../dist'),
-        publicPath: ''
+        publicPath: '/public/'
     },
+    devtool: 'inline-source-map',
     module: { 
         rules: [
             {
@@ -31,3 +33,26 @@ module.exports = {
         })
     ]
 }
+if (isDev) {
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    }
+    config.devServer = {
+        host: '0.0.0.0',
+        port: '8888',
+        contentBase: config.output.path,
+        hot: true,
+        overlay: {
+            errors: true
+        },
+        publicPath: '/public/',
+        historyApiFallback: {
+            index: '/public/index.html'
+        }
+    }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+module.exports = config;
--- client/app.js
+++ client/app.js
@@ -1,5 +1,21 @@
 import ReactDom from 'react-dom'
 import React from 'react';
 import App from './App.jsx';
+import {AppContainer} from 'react-hot-loader'
 
-ReactDom.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+const render = Component => {
+    ReactDom.hydrate(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        root
+    )
+}
+render(App);
+if (module.hot) {
+    module.hot.accept('./App.jsx', () => {
+        const NextApp = require('./App.jsx').default;
+        render(NextApp);
+    })
+}
